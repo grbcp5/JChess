b5f0b98013925ba98b0577d26b2060dc7c8e095a
= <=>
2017-03-01 14:18:04 -0600

    Add implementation to the abstract Piece class
    Most significantly the getLegalMoves function

--- src/net/ddns/grbcp5/chess/engine/pieces/Piece.java
+++ src/net/ddns/grbcp5/chess/engine/pieces/Piece.java
@@ -1,7 +1,27 @@
 package net.ddns.grbcp5.chess.engine.pieces;
 
+import net.ddns.grbcp5.chess.engine.Alliance;
+import net.ddns.grbcp5.chess.engine.board.Board;
+import net.ddns.grbcp5.chess.engine.board.Move;
+
+import java.util.List;
+
 /**
  * Created by GrantBroadwater on 3/1/17.
  */
-public class Piece {
+public abstract class Piece
+{
+
+    protected final int position;
+    protected final Alliance alliance;
+
+
+    public Piece(final int position, final Alliance alliance)
+    {
+        this.position = position;
+        this.alliance = alliance;
+    }
+
+    public abstract List<Move> getLegalMoves(final Board board);
+    
 }
