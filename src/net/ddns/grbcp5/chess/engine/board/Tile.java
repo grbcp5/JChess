fb86972c77a7c196769f3f1a215f5e05979fdcc7
= <=>
2017-03-01 13:06:01 -0600

    Create abstract Piece class

--- src/net/ddns/grbcp5/chess/engine/board/Tile.java
+++ src/net/ddns/grbcp5/chess/engine/board/Tile.java
@@ -1,7 +1,22 @@
 package net.ddns.grbcp5.chess.engine.board;
 
+import net.ddns.grbcp5.chess.engine.pieces.Piece;
+
 /**
  * Created by GrantBroadwater on 3/1/17.
  */
-public class Tile {
+public abstract class Tile {
+
+    // Tile coordinate location from 0 - 63
+    int tileCoordinate;
+
+    public Tile( int tileCoordinate )
+    {
+        this.tileCoordinate = tileCoordinate;
+    }
+
+    public abstract boolean isOccupied();
+
+    public abstract Piece getPiece();
+
 }
